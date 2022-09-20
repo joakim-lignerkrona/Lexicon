95bcae5f5598129a5f312cb0cea2f22217281c70
Jockan <joaklign100@student.kau.se>
2022-09-20 16:16:40 +0200

    web output and and input

--- Laboration-4/index.js
+++ Laboration-4/index.js
@@ -1,5 +1,16 @@
-var number1 = 5
-var number2 = 4
+
+var runningInBrowser
+try {
+    runningInBrowser = window ? true : false
+} catch (error) {
+    runningInBrowser = false
+}
+
+//set inputvärlde från eventuell querystring
+var number1 = runningInBrowser ? parseInt(getQueryVariable('n1')) : undefined
+var number2 = runningInBrowser ? parseInt(getQueryVariable('n2')) : undefined
+number1 ? '' : number1 = 5
+number2 ? '' : number2 = 4
 
 console.log(number1 + number2)
 console.log(number1 - number2)
@@ -13,3 +24,39 @@ let person = {
 }
 
 console.log(`Hej mitt namn är ${person.förnamn} ${person.efternamn} och jag är ${person.ålder} år gammal`)
+
+if (runningInBrowser)
+    try {
+        document.writeln('<div id="console">')
+        document.writeln('<pre><code>')
+        document.writeln(number1 + number2)
+        document.writeln(number1 - number2)
+        document.writeln(number1 * number2)
+        document.writeln(`${number1} plus ${number2} är: ${number1 + number2}`)
+        document.writeln('</code></pre>')
+        document.writeln('<pre><code>')
+        document.writeln(`Hej mitt namn är ${person.förnamn} ${person.efternamn} och jag är ${person.ålder} år gammal`)
+        document.writeln('</code></pre>')
+        document.writeln('</div>')
+    } catch (error) {
+        console.log('Kör i webbläsaren för full funktionallitet')
+    }
+
+
+
+function getQueryVariable(variable) {
+    try {
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split('=');
+            if (decodeURIComponent(pair[0]) == variable) {
+                return decodeURIComponent(pair[1]);
+            }
+        }
+        console.log('Query variable %s not found', variable);
+    } catch (error) {
+        return NaN
+    }
+
+}
