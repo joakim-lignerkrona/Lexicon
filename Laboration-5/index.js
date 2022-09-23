a181e339e0693107dc9c360797b60f31d69818ad
Jockan <joaklign100@student.kau.se>
2022-09-23 10:02:17 +0200

    arrow functions

--- Laboration-5/index.js
+++ Laboration-5/index.js
@@ -2,11 +2,7 @@ var names = ["Kalle", "Lisa", "Erik", "Maria", "Anna"];
 names.pop()
 names[1] = "Kajsa";
 names.push("Joakim");
-names.sort(function (a, b) {
-    return a.localeCompare(b);
-});
+names.sort((a, b) => a.localeCompare(b));
 let indexOfJoakim = names.indexOf("Joakim")
 names.splice(indexOfJoakim, 1);
-for (var i = 0; i < names.length; i++) {
-    console.log(names[i]);
-}
+names.forEach(name => console.log(name));
