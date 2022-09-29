a25aa7dd020ca411134d8866dadc8f8d3b2ca134
Jockan <joaklign100@student.kau.se>
2022-09-29 23:21:50 +0200

    lab 8 part 2

--- Laboration-8/script.js
+++ Laboration-8/script.js
@@ -1,8 +1,76 @@
 let formInputs = document.querySelectorAll("input")
+let select = document.querySelector("select")
+const submitButton = document.querySelector("button");
+let post = {}
+
 formInputs.forEach((input) => {
-    input.addEventListener("on", (event) => {
+    post[input.name] = { value: undefined, element: input }
+    if (input.type === "submit") {
+        input.addEventListener("click", (event) => {
+            event.preventDefault()
+            validateForm(post)
+        })
+        return
+    }
+
+
+    input.addEventListener("input", (event) => {
         let input = event.target
         let value = input.value
-        console.log(value)
+        if (input.type === "submit") {
+            return
+        }
+        if (input.checked) {
+            post[input.name] = { value: true, element: input }
+            return
+        }
+        else if (!input.checked && input.type === "checkbox") {
+            post[input.name] = { value: false, element: input }
+            return
+        }
+        post[input.name] = { value, element: input }
+        validateRow(input, value)
     })
 })
+select.addEventListener("change", (event) => {
+    let input = event.target
+    let value = input.value
+    post[input.name] = { value, element: input }
+})
+post[select.name] = { value: undefined, element: select }
+function validateForm(posts) {
+    let errors = []
+    for (const key in posts) {
+        if (posts.hasOwnProperty(key)) {
+            const post = posts[key];
+            post.element.classList.remove("is-invalid")
+            if (post.value === undefined || post.value === "") {
+                if (post.element.name !== "submit") {
+                    errors.push(post.element)
+                }
+            } else {
+                post.element.classList.add("is-valid")
+            }
+        }
+    }
+    errors.forEach((error) => {
+        error.classList.add("is-invalid")
+    })
+    if (errors.length === 0) {
+        console.log("no errors");
+        submitForm(posts)
+    }
+
+}
+function submitForm(posts) {
+    console.table(posts);
+}
+function validateRow(input, value) {
+    if (value === undefined || value === "") {
+        /* input.classList.remove("is-valid")
+        input.classList.add("is-invalid") */
+    } else {
+        input.classList.remove("is-invalid")
+        input.classList.add("is-valid")
+    }
+}
